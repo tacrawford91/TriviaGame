409b2dbc7eb05823ebe61b20226ff56ef7aca0c3
unknown <tacrawford91@gmail.com>
2017-12-16 16:32:17 -0600

    finished win/loss logic, including reset timer. Add beginning styling, testing theme. Still need opening start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,8 @@ var trivia = {
     b:["blue", "hello Dog"],
     c:["red", "Whatsup Dawg"],
     d:["yellow", "bad dog"],
-    answer:["b" ,"c"]
+    answer:["b" ,"c"],
+    image: ["./../images/placeHolder.JPG","./../images/placeHolder.JPG"]
 }
 
 // var superTrivia = [
@@ -37,51 +38,79 @@ var trivia = {
 //     $(".d").append($("<div>").html(`<h1> ${trivia.d[i]} </h1>`));
 // }
 
+//intial timer
+var timeRemaining = 30;
 //intial questionNumber 
-var timeRemaining = 10;
 var questionNumber = 0;
 var usersChoice;
-//start timer
-
-var timerValue = setInterval(function() {
+var timerValue;
+//functions
+//set interval for timer
+function startTimer() {
+    //Stop timer if it equals zero 
     if (timeRemaining === 1) {
         clearInterval(timerValue)
         console.log("times Up!")
     }
     timeRemaining = timeRemaining - 1
     $(".timer").html(`Time Remaining: ${timeRemaining}`)
-},1000)
-
+}
+//Generate html for each question
 function nextQuestion() {
+    //Generate html for each question
     $(".question").html(`<h1> ${trivia.question[questionNumber]} </h1>`);
     $(".a").attr("guess", "a").html(`<h1> ${trivia.a[questionNumber]} </h1>`);
     $(".b").attr("guess", "b").html(`<h1> ${trivia.b[questionNumber]} </h1>`);
     $(".c").attr("guess", "c").html(`<h1> ${trivia.c[questionNumber]} </h1>`);
     $(".d").attr("guess", "d").html(`<h1> ${trivia.d[questionNumber]} </h1>`);
+    //Update Qeustion Timer
     timeRemaining = 30;
+    //Start CountDown
+    timerValue = setInterval(startTimer, 1000);
 };
-
+function emptyHTML(){
+    $(".question").html("");
+    $(".a").attr("guess", "a").html("");
+    $(".b").attr("guess", "b").html("");
+    $(".c").attr("guess", "c").html("");
+    $(".d").attr("guess", "d").html("");
+}
+function congrats() {
+    emptyHTML()
+    $(".question").html("<h1> Congrats </h1>");
+    //display correct answer
+     //generate questionNumber Image
+}
+function loser() {
+    emptyHTML()
+    $(".question").html("<h1> Wrong </h1>");
+    //display correct answer
+     //generate questionNumber Image
+}
+//START APP
 nextQuestion();
 
 //listen for users guess
-
 $(".choice").on("click", function(event){ 
         usersChoice = $(this)
         // compare choice to answer 
         if ( usersChoice.attr("guess") === trivia.answer[questionNumber]) {
             console.log("CORRECT!!!!");
-            questionNumber++;
-            //Show Congrats for set timer
+            //Show Congrats
+            clearInterval(timerValue)
+            congrats();
             //generate new questions & reset questionTimer
-            nextQuestion();
-
+            questionNumber++;
+            setTimeout(nextQuestion,10000)
         } else {
             console.log("YOU LOSE");
-            questionNumber++;
-            //Show you lose for set time
+            //Show you lose
+            clearInterval(timerValue);
+            loser();
             //generate questions & reset questionTimer
-            nextQuestion();
-        }
+            questionNumber++;
+            setTimeout(nextQuestion,10000)
+        } 
 
         
 
